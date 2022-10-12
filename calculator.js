7ca6deb5c0c02e0f801542c47a6a49f59ce04c7e
catherinebarrientos <barrientoscb05@gmail.com>
2022-10-12 23:13:06 +0800

    initial push

---
+++ calculator.js
@@ -0,0 +1,36 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operation){
+    let result;
+    switch(operation){
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            console.log("ERR");
+        return result;
+    }
+}
